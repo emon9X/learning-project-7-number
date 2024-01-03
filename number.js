87306635d65a399cb954232f84cc0024e63ab185
emon9X <shahariar.emn990@gmail.com>
2024-01-03 12:44:33 +0600

    second commit

--- number.js
+++ number.js
@@ -95,3 +95,57 @@ console.log(typeof Infinity);
     console.log(x, y, z)
 }
 // --------------------------------------------------------------------------------
+// number methods 
+/*Method	    Description
+toString()	    Returns a number as a string
+toExponential()	Returns a number written in exponential notation
+toFixed()	    Returns a number written with a number of decimals
+toPrecision()	Returns a number written with a specified length
+ValueOf()	    Returns a number as a number*/
+
+
+//toString method
+{
+    let x = 123;
+    console.log(x.toString());
+    console.log((123).toString());
+    console.log((100 + 23).toString());
+}
+
+//toExponential method
+//exponential or scientific representation of number 
+//The toExponential() method returns a string
+// The parameter is optional. If you don't specify it, JavaScript will not round the number.
+//The parameter specifies the number of decimal digits after decimal point.
+{
+    let x = 123;
+    console.log(x.toExponential(2)); // 1.23x10^2 = 1.23e+2
+    console.log(x.toExponential(3)); // 1.230x10^2 = 1.230e+2
+}
+{
+    let num = 123456789;
+    let exponential = num.toExponential(); // This converts the number to exponential notation
+    console.log(exponential); // Output will be "1.23456789e+8"
+
+}
+
+//toFixed(digits after decimal point)
+//toFixed() returns a string
+{
+    let num = 9.389;
+    console.log(num.toFixed(1));
+    console.log(num.toFixed(2));
+    console.log(num.toFixed(4));
+}
+
+//toPrecision(length of digits)
+{
+    let num = 123.456789;
+    console.log(num.toPrecision(2));
+    console.log(num.toPrecision(3));
+    console.log(num.toPrecision(4));
+    console.log(num.toPrecision(5));
+}
+
+//valueOf method
+//The valueOf() method is used internally in JavaScript to convert Number objects to primitive values.
